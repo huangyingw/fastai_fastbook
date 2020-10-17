c144a285369815b9595d2bc2f92b24e51691da86
Ying Huang <huangyingw@gmail.com>
2020-10-17 16:41:42 -0700

    n

--- 15_arch_details.py
+++ 15_arch_details.py
@@ -15,13 +15,13 @@
 #     name: python3
 # ---
 
-#hide
-# !pip install -Uqq fastbook
+# hide
+from fastai.vision.all import *
+from fastbook import *
 import fastbook
 fastbook.setup_book()
 
-#hide
-from fastbook import *
+# hide
 
 # + active=""
 # [[chapter_arch_details]]
@@ -51,8 +51,8 @@ model_meta[resnet50]
 
 # If we take all of the layers prior to the cut point of `-2`, we get the part of the model that fastai will keep for transfer learning. Now, we put on our new head. This is created using the function `create_head`:
 
-#hide_output
-create_head(20,2)
+# hide_output
+create_head(20, 2)
 
 # ```
 # Sequential(
@@ -108,23 +108,24 @@ create_head(20,2)
 # ### A Siamese Network
 
 # +
-#hide
-from fastai.vision.all import *
+# hide
 path = untar_data(URLs.PETS)
-files = get_image_files(path/"images")
+files = get_image_files(path / "images")
 
 class SiameseImage(fastuple):
-    def show(self, ctx=None, **kwargs): 
-        img1,img2,same_breed = self
+    def show(self, ctx=None, **kwargs):
+        img1, img2, same_breed = self
         if not isinstance(img1, Tensor):
-            if img2.size != img1.size: img2 = img2.resize(img1.size)
-            t1,t2 = tensor(img1),tensor(img2)
-            t1,t2 = t1.permute(2,0,1),t2.permute(2,0,1)
-        else: t1,t2 = img1,img2
+            if img2.size != img1.size:
+                img2 = img2.resize(img1.size)
+            t1, t2 = tensor(img1), tensor(img2)
+            t1, t2 = t1.permute(2, 0, 1), t2.permute(2, 0, 1)
+        else:
+            t1, t2 = img1, img2
         line = t1.new_zeros(t1.shape[0], t1.shape[1], 10)
-        return show_image(torch.cat([t1,line,t2], dim=2), 
+        return show_image(torch.cat([t1, line, t2], dim=2),
                           title=same_breed, ctx=ctx)
-    
+
 def label_func(fname):
     return re.match(r'^(.*)_\d+.jpg$', fname.name).groups()[0]
 
@@ -134,23 +135,24 @@ class SiameseTransform(Transform):
         self.lbl2files = {l: L(f for f in files if label_func(f) == l) for l in self.labels}
         self.label_func = label_func
         self.valid = {f: self._draw(f) for f in files[splits[1]]}
-        
+
     def encodes(self, f):
-        f2,t = self.valid.get(f, self._draw(f))
-        img1,img2 = PILImage.create(f),PILImage.create(f2)
+        f2, t = self.valid.get(f, self._draw(f))
+        img1, img2 = PILImage.create(f), PILImage.create(f2)
         return SiameseImage(img1, img2, t)
-    
+
     def _draw(self, f):
         same = random.random() < 0.5
         cls = self.label_func(f)
-        if not same: cls = random.choice(L(l for l in self.labels if l != cls)) 
-        return random.choice(self.lbl2files[cls]),same
-    
+        if not same:
+            cls = random.choice(L(l for l in self.labels if l != cls))
+        return random.choice(self.lbl2files[cls]), same
+
 splits = RandomSplitter()(files)
 tfm = SiameseTransform(files, label_func, splits)
 tls = TfmdLists(files, tfm, splits=splits)
-dls = tls.dataloaders(after_item=[Resize(224), ToTensor], 
-    after_batch=[IntToFloatTensor, Normalize.from_stats(*imagenet_stats)])
+dls = tls.dataloaders(after_item=[Resize(224), ToTensor],
+                      after_batch=[IntToFloatTensor, Normalize.from_stats(*imagenet_stats)])
 
 
 # -
@@ -161,8 +163,8 @@ dls = tls.dataloaders(after_item=[Resize(224), ToTensor],
 
 class SiameseModel(Module):
     def __init__(self, encoder, head):
-        self.encoder,self.head = encoder,head
-    
+        self.encoder, self.head = encoder, head
+
     def forward(self, x1, x2):
         ftrs = torch.cat([self.encoder(x1), self.encoder(x2)], dim=1)
         return self.head(ftrs)
@@ -174,7 +176,7 @@ encoder = create_body(resnet34, cut=-2)
 
 # Then we can create our head. A look at the encoder tells us the last layer has 512 features, so this head will need to receive `512*4`. Why 4? First we have to multiply by 2 because we have two images. Then we need a second multiplication by 2 because of our concat-pool trick. So we create the head as follows:
 
-head = create_head(512*4, 2, ps=0.5)
+head = create_head(512 * 4, 2, ps=0.5)
 
 # With our encoder and head, we can now build our model:
 
@@ -187,7 +189,7 @@ def loss_func(out, targ):
     return nn.CrossEntropyLoss()(out, targ.long())
 
 
-# More importantly, to take full advantage of transfer learning, we have to define a custom *splitter*. A splitter is a function that tells the fastai library how to split the model into parameter groups. These are used behind the scenes to train only the head of a model when we do transfer learning. 
+# More importantly, to take full advantage of transfer learning, we have to define a custom *splitter*. A splitter is a function that tells the fastai library how to split the model into parameter groups. These are used behind the scenes to train only the head of a model when we do transfer learning.
 #
 # Here we want two parameter groups: one for the encoder and one for the head. We can thus define the following splitter (`params` is just a function that returns all parameters of a given module):
 
@@ -197,7 +199,7 @@ def siamese_splitter(model):
 
 # Then we can define our `Learner` by passing the data, model, loss function, splitter, and any metric we want. Since we are not using a convenience function from fastai for transfer learning (like `cnn_learner`), we have to call `learn.freeze` manually. This will make sure only the last parameter group (in this case, the head) is trained:
 
-learn = Learner(dls, model, loss_func=loss_func, 
+learn = Learner(dls, model, loss_func=loss_func,
                 splitter=siamese_splitter, metrics=accuracy)
 learn.freeze()
 
@@ -208,7 +210,7 @@ learn.fit_one_cycle(4, 3e-3)
 # Before unfreezing and fine-tuning the whole model a bit more with discriminative learning rates (that is: a lower learning rate for the body and a higher one for the head):
 
 learn.unfreeze()
-learn.fit_one_cycle(4, slice(1e-6,1e-4))
+learn.fit_one_cycle(4, slice(1e-6, 1e-4))
 
 # 94.8\% is very good when we remember a classifier trained the same way (with no data augmentation) had an error rate of 7%.
 
@@ -254,7 +256,7 @@ learn.fit_one_cycle(4, slice(1e-6,1e-4))
 # ```
 #
 # is just testing whether there are any embeddings to deal with—we can skip this section if we only have continuous variables. `self.embeds` contains the embedding matrices, so this gets the activations of each:
-#  
+#
 # ```python
 #     x = [e(x_cat[:,i]) for i,e in enumerate(self.embeds)]
 # ```
@@ -346,5 +348,3 @@ learn.fit_one_cycle(4, slice(1e-6,1e-4))
 # 1. Write your own custom splitter to create a separate parameter group for every ResNet block, and a separate group for the stem. Try training with it, and see if it improves the pet recognizer.
 # 1. Read the online chapter about generative image models, and create your own colorizer, super-resolution model, or style transfer model.
 # 1. Create a custom head using nearest neighbor interpolation and use it to do segmentation on CamVid.
-
-
