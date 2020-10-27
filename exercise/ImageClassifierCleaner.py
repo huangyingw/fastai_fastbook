66d1cc641290fc983b891568e5372e8dc942c135
Ying Huang <huangyingw@gmail.com>
2020-10-26 21:48:01 -0700

    n

---
+++ exercise/ImageClassifierCleaner.py
@@ -0,0 +1,40 @@
+from fastai.vision.widgets import *
+from fastbook import *
+import fastbook
+fastbook.setup_book()
+
+
+
+path = Path('bears')
+
+
+bears = DataBlock(
+    blocks=(ImageBlock, CategoryBlock),
+    get_items=get_image_files,
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=Resize(128))
+
+
+# ## Training Your Model, and Using It to Clean Your Data
+
+bears = bears.new(
+    item_tfms=RandomResizedCrop(224, min_scale=0.5),
+    batch_tfms=aug_transforms())
+dls = bears.dataloaders(path)
+
+learn = cnn_learner(dls, resnet18, metrics=error_rate)
+learn.fine_tune(4)
+
+interp = ClassificationInterpretation.from_learner(learn)
+interp.plot_confusion_matrix()
+
+interp.plot_top_losses(5, nrows=1)
+
+cleaner = ImageClassifierCleaner(learn)
+cleaner
+
+# +
+# hide
+# for idx in cleaner.delete(): cleaner.fns[idx].unlink()
+# for idx,cat in cleaner.change(): shutil.move(str(cleaner.fns[idx]), path/cat)
