9d8b67c379899779b13174e674626cf4ab7fc58a
Ying Huang <huangyingw@gmail.com>
2020-10-29 21:32:49 -0700

    n

--- exercise/presizing.py
+++ exercise/presizing.py
@@ -17,14 +17,6 @@ fname = (path / "images").ls()[0]
 
 re.findall(r'(.+)_\d+.jpg$', fname.name)
 
-pets = DataBlock(blocks=(ImageBlock, CategoryBlock),
-                 get_items=get_image_files,
-                 splitter=RandomSplitter(seed=42),
-                 get_y=using_attr(RegexLabeller(r'(.+)_\d+.jpg$'), 'name'),
-                 item_tfms=Resize(460),
-                 batch_tfms=aug_transforms(size=224, min_scale=0.75))
-dls = pets.dataloaders(path / "images")
-
 
 # +
 dblock1 = DataBlock(blocks=(ImageBlock(), CategoryBlock()),
