be4ab9ed38c5a8588574dfaa09023afdc2b071f6
Ying Huang <huangyingw@gmail.com>
2020-10-30 21:39:00 -0700

    n

--- exercise/unfreeze.py
+++ exercise/unfreeze.py
@@ -4,6 +4,14 @@ from fastbook import *
 import fastbook
 fastbook.setup_book()
 
+path = untar_data(URLs.PETS)
+pets = DataBlock(blocks=(ImageBlock, CategoryBlock),
+                 get_items=get_image_files,
+                 splitter=RandomSplitter(seed=42),
+                 get_y=using_attr(RegexLabeller(r'(.+)_\d+.jpg$'), 'name'),
+                 item_tfms=Resize(460),
+                 batch_tfms=aug_transforms(size=224, min_scale=0.75))
+dls = pets.dataloaders(path / "images")
 
 learn = cnn_learner(dls, resnet34, metrics=error_rate)
 learn.fit_one_cycle(3, 3e-3)
