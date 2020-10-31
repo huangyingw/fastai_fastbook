6f9a332955e55aa7b52f48cecb9d2651c5286bb2
Ying Huang <huangyingw@gmail.com>
2020-10-31 16:55:42 -0700

    n

--- exercise/most_confused.py
+++ exercise/most_confused.py
@@ -1,3 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     split_at_heading: true
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
 from fastai.callback.fp16 import *
 from fastai.vision.all import *
 from fastbook import *
@@ -7,7 +22,7 @@ fastbook.setup_book()
 path = untar_data(URLs.PETS)
 pets = DataBlock(blocks=(ImageBlock, CategoryBlock),
                  get_items=get_image_files,
-                 splitter=RandomSubsetSplitter(train_sz=0.05, valid_sz=0.01, seed=42),
+                 splitter=RandomSubsetSplitter(train_sz=0.3, valid_sz=0.1, seed=42),
                  get_y=using_attr(RegexLabeller(r'(.+)_\d+.jpg$'), 'name'),
                  item_tfms=Resize(460),
                  batch_tfms=aug_transforms(size=224, min_scale=0.75))
@@ -16,9 +31,9 @@ dls = pets.dataloaders(path / "images")
 learn = cnn_learner(dls, resnet34, metrics=error_rate)
 learn.fine_tune(1)
 
+learn.recorder.plot_loss()
+
 interp = ClassificationInterpretation.from_learner(learn)
 interp.plot_confusion_matrix(figsize=(12, 12), dpi=60)
 
 interp.most_confused(min_val=5)
-
-learn.recorder.plot_loss()
