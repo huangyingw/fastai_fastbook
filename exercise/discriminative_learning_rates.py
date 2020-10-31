f02a7e2f68dfbf60a5b2cb9e553749a6f5f9cafe
Ying Huang <huangyingw@gmail.com>
2020-10-31 16:55:56 -0700

    n

--- exercise/discriminative_learning_rates.py
+++ exercise/discriminative_learning_rates.py
@@ -1,3 +1,17 @@
+from fastai.callback.fp16 import *
+from fastai.vision.all import *
+from fastbook import *
+import fastbook
+fastbook.setup_book()
+
+path = untar_data(URLs.PETS)
+pets = DataBlock(blocks=(ImageBlock, CategoryBlock),
+                 get_items=get_image_files,
+                 splitter=RandomSplitter(seed=42),
+                 get_y=using_attr(RegexLabeller(r'(.+)_\d+.jpg$'), 'name'),
+                 item_tfms=Resize(460),
+                 batch_tfms=aug_transforms(size=224, min_scale=0.75))
+dls = pets.dataloaders(path / "images")
 
 learn = cnn_learner(dls, resnet34, metrics=error_rate)
 learn.fit_one_cycle(3, 3e-3)
