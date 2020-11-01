824728682975628a16568ef2abb470f7a5895968
Ying Huang <huangyingw@gmail.com>
2020-10-31 17:02:48 -0700

    n

--- exercise/presizing.py
+++ exercise/presizing.py
@@ -1,3 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     split_at_heading: true
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
 from fastai.callback.fp16 import *
 from fastai.vision.all import *
 from fastbook import *
