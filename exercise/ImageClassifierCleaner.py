7a1ea3b48e19f47ac484af62acaf779ec03690a0
Ying Huang <huangyingw@gmail.com>
2020-10-31 21:38:16 -0700

    n

--- exercise/ImageClassifierCleaner.py
+++ exercise/ImageClassifierCleaner.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     split_at_heading: true
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# hide
 from fastai.vision.widgets import *
 from fastbook import *
 import fastbook
