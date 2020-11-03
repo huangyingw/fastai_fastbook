74829c02cc4c0b3fdcad9943eb922bc7ae74b095
Ying Huang <huangyingw@gmail.com>
2020-11-02 16:08:22 -0800

    n

--- exercise/basicoptim.py
+++ exercise/basicoptim.py
@@ -1,3 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
 from fastbook import *
 from fastai.vision.all import *
 import fastbook
