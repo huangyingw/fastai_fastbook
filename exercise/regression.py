15be05ca2cdd54596682640d9abc781bef6c8717
Ying Huang <huangyingw@gmail.com>
2020-11-02 16:10:36 -0800

    n

--- exercise/regression.py
+++ exercise/regression.py
@@ -4,3 +4,20 @@ import fastbook
 fastbook.setup_book()
 
 path = untar_data(URLs.BIWI_HEAD_POSE)
+path.ls().sorted()
+
+(path / '01').ls().sorted()
+
+img_files = get_image_files(path)
+def img2pose(x): return Path(f'{str(x)[:-7]}pose.txt')
+
+
+img2pose(img_files[0])
+
+im = PILImage.create(img_files[0])
+im.shape
+
+im.to_thumb(160)
+
+cal = np.genfromtxt(path / '01' / 'rgb.cal', skip_footer=6)
+cal
