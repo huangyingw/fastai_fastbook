1a2244c74d6acc22a6ee6f68b7356e0e38fbd88b
Ying Huang <huangyingw@gmail.com>
2020-11-26 13:05:45 -0800

    n

--- exercise/principal_component_analysis.py
+++ exercise/principal_component_analysis.py
@@ -1,3 +1,52 @@
+from fastai.tabular.all import *
+from fastai.collab import *
+from fastbook import *
+import fastbook
+fastbook.setup_book()
+
+path = untar_data(URLs.ML_100k)
+
+ratings = pd.read_csv(path / 'u.data', delimiter='\t', header=None,
+                      names=['user', 'movie', 'rating', 'timestamp'])
+ratings.head()
+
+movies = pd.read_csv(path / 'u.item', delimiter='|', encoding='latin-1',
+                     usecols=(0, 1), names=('movie', 'title'), header=None)
+movies.head()
+
+ratings = ratings.merge(movies)
+ratings.head()
+
+dls = CollabDataLoaders.from_df(ratings, item_name='title', bs=64)
+dls.show_batch()
+
+dls.classes
+
+n_users = len(dls.classes['user'])
+n_movies = len(dls.classes['title'])
+
+
+def create_params(size):
+    return nn.Parameter(torch.zeros(*size).normal_(0, 0.01))
+
+class DotProductBias(Module):
+    def __init__(self, n_users, n_movies, n_factors, y_range=(0, 5.5)):
+        self.user_factors = create_params([n_users, n_factors])
+        self.user_bias = create_params([n_users])
+        self.movie_factors = create_params([n_movies, n_factors])
+        self.movie_bias = create_params([n_movies])
+        self.y_range = y_range
+
+    def forward(self, x):
+        users = self.user_factors[x[:, 0]]
+        movies = self.movie_factors[x[:, 1]]
+        res = (users * movies).sum(dim=1)
+        res += self.user_bias[x[:, 0]] + self.movie_bias[x[:, 1]]
+        return sigmoid_range(res, *self.y_range)
+
+model = DotProductBias(n_users, n_movies, 50)
+learn = Learner(dls, model, loss_func=MSELossFlat())
+learn.fit_one_cycle(5, 5e-3, wd=0.1)
 
 g = ratings.groupby('title')['rating'].count()
 top_movies = g.sort_values(ascending=False).index.values[:1000]
