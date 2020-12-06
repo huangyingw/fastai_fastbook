ff574425c6a7eae48ed4819f6a04c1a2b933f9b7
Ying Huang <huangyingw@gmail.com>
2020-12-05 20:52:31 -0800

    n

---
+++ exercise/embedding_distance.py
@@ -0,0 +1,12 @@
+from fastai.tabular.all import *
+from fastai.collab import *
+from fastbook import *
+import fastbook
+fastbook.setup_book()
+
+
+movie_factors = learn.model.i_weight.weight
+idx = dls.classes['title'].o2i['Silence of the Lambs, The (1991)']
+distances = nn.CosineSimilarity(dim=1)(movie_factors, movie_factors[idx][None])
+idx = distances.argsort(descending=True)[1]
+dls.classes['title'][idx]
