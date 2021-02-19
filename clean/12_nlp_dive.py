4f037861143500d962259f176acd2626196da026
Ying Huang <huangyingw@gmail.com>
2021-02-18 22:14:05 -0800

    n

--- clean/12_nlp_dive.py
+++ clean/12_nlp_dive.py
@@ -241,14 +241,14 @@ class LSTMCell(Module):
 
     def forward(self, input, state):
         h, c = state
-        h = torch.stack([h, input], dim=1)
+        h = torch.cat([h, input], dim=1)
         forget = torch.sigmoid(self.forget_gate(h))
         c = c * forget
         inp = torch.sigmoid(self.input_gate(h))
         cell = torch.tanh(self.cell_gate(h))
         c = c + inp * cell
         out = torch.sigmoid(self.output_gate(h))
-        h = outgate * torch.tanh(c)
+        h = out * torch.tanh(c)
         return h, (h, c)
 
 
