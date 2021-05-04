b606dcdadd913899bd1c5c60d6ef9fd8fd6732ba
Ying Huang <huangyingw@gmail.com>
2021-05-03 21:49:36 -0700

    n

--- 15_arch_details.py
+++ 15_arch_details.py
@@ -2,13 +2,12 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py
 #     split_at_heading: true
 #     text_representation:
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.6.0
+#       jupytext_version: 1.11.1
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -112,7 +111,6 @@ create_head(20, 2)
 path = untar_data(URLs.PETS)
 files = get_image_files(path / "images")
 
-
 class SiameseImage(fastuple):
     def show(self, ctx=None, **kwargs):
         img1, img2, same_breed = self
@@ -127,10 +125,8 @@ class SiameseImage(fastuple):
         return show_image(torch.cat([t1, line, t2], dim=2),
                           title=same_breed, ctx=ctx)
 
-
 def label_func(fname):
     return re.match(r'^(.*)_\d+.jpg$', fname.name).groups()[0]
-
 
 class SiameseTransform(Transform):
     def __init__(self, files, label_func, splits):
@@ -150,7 +146,6 @@ class SiameseTransform(Transform):
         if not same:
             cls = random.choice(L(l for l in self.labels if l != cls))
         return random.choice(self.lbl2files[cls]), same
-
 
 splits = RandomSplitter()(files)
 tfm = SiameseTransform(files, label_func, splits)
@@ -178,9 +173,9 @@ class SiameseModel(Module):
 
 encoder = create_body(resnet34, cut=-2)
 
-# Then we can create our head. A look at the encoder tells us the last layer has 512 features, so this head will need to receive `512*4`. Why 4? First we have to multiply by 2 because we have two images. Then we need a second multiplication by 2 because of our concat-pool trick. So we create the head as follows:
+# Then we can create our head. A look at the encoder tells us the last layer has 512 features, so this head will need to receive `512*2`. Why 2? We have to multiply by 2 because we have two images. So we create the head as follows:
 
-head = create_head(512 * 4, 2, ps=0.5)
+head = create_head(512 * 2, 2, ps=0.5)
 
 # With our encoder and head, we can now build our model:
 
@@ -271,7 +266,7 @@ learn.fit_one_cycle(4, slice(1e-6, 1e-4))
 #     x = torch.cat(x, 1)
 # ```
 #
-# Then dropout is applied. You can pass `emb_drop` to `__init__` to change this value:
+# Then dropout is applied. You can pass `embd_p` to `__init__` to change this value:
 #
 # ```python
 #     x = self.emb_drop(x)
